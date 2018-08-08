4c6294fe596eb9d9a567dd9e9eef6bdc8635756e
stepboman <38188185+stepboman@users.noreply.github.com>
2018-08-08 08:15:33 -0500

    rev1

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- Creating seed data for the burgers_db database --
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Flame Burger', true);
